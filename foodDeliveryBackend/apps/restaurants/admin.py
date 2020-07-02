1ab36de1b5d1ab98122cf583ad50ed5b6ea81c8f
Bakhrom Rakhmonov <alex960126@gmail.com>
2020-07-02 17:53:12 +0500

    Update admin.py
    rest admin

--- foodDeliveryBackend/apps/restaurants/admin.py
+++ foodDeliveryBackend/apps/restaurants/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.restaurants.models import Restaurant
 
 # Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'user')
+    list_filter = ('user', )
+  
