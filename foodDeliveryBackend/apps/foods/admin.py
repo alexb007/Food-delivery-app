f819069e412794823229df7ecfdc3dd188c56664
Bakhrom Rakhmonov <alex960126@gmail.com>
2020-07-02 17:50:18 +0500

    Update admin.py
    admin models

--- foodDeliveryBackend/apps/foods/admin.py
+++ foodDeliveryBackend/apps/foods/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from apps.foods.models import FoodCategory, Food,
+
 # Register your models here.
+@admin.register(FoodCategory)
+class FoodCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'restaurant')
+    list_filter = ('restaurant')
+    
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'restaurant', 'active', 'price')
+    list_filter = ( 'category', 'restaurant')
+    
