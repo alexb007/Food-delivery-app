0fa270d7a940a66b47da5fe42e8b16ba4cd919f3
alexb007 <alex960126@icloud.com>
2020-07-08 04:48:46 +0500

    static root set

--- foodDeliveryBackend/apps/users/admin.py
+++ foodDeliveryBackend/apps/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from apps.users.models import User
 # Register your models here.
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-   pass
+admin.site.register(User, UserAdmin)
