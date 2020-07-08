43e1ff6b92775b131e46277dcdd5638b92139292
alexb007 <alex960126@icloud.com>
2020-07-08 05:49:28 +0500

    static root set

--- foodDeliveryBackend/apps/restaurants/admin.py
+++ foodDeliveryBackend/apps/restaurants/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from apps.restaurants.models import Restaurant
+from apps.restaurants.models import Restaurant, RestaurantType
+
 
 # Register your models here.
+
+@admin.register(RestaurantType)
+class RestaurantTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('user',)
+
+
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'user')
-    list_filter = ('user', )
-  
+    list_display = ('name', 'address', 'user', 'type', 'opens', 'closes')
+    list_filter = ('user', 'type')
